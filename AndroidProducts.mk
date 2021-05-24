0e022d405cbce87d0ed5e286d4cc02a8d8ae2583
ganeshi4u <ganeshvarmai4u@gmail.com>
2021-05-24 11:58:15 +0200

    psyche: Initial tree from lisa
    Co-authored-by: kubersharma001 <kubersharma001@gmail.com>
    Co-authored-by: palaych <palaych@arrowos.net>
    Co-authored-by: Lineu Martins <lineumartins001@gmail.com>
    
    Change-Id: I64aad6415c327957ebed713e75230864eb3d2066
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2021 LineageOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_psyche.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_psyche-eng \
+    lineage_psyche-userdebug \
+    lineage_psyche-user
