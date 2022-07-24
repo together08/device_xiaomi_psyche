adc3388e87c17fb4046aedd931dde796b1c5663d
together08 <together08@yeah.net>
2022-07-24 11:19:45 +0800

    [DNM] psyche: Also force adb secure to 0 on userdebug builds

--- properties/default.mk
+++ properties/default.mk
@@ -6,3 +6,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0
 endif
+
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it in /product on userdebug builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
